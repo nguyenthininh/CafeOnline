2c51604fb698a54abd907458493cb29709c95cb8
Nina Nguyen <Ninh2000md>
2020-11-15 19:37:42 +0700

    asm

--- CafeOnline/Page/Cart.cshtml.cs
+++ CafeOnline/Page/Cart.cshtml.cs
@@ -1,16 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CafeOnline.Infrastructure;
+using CafeOnline.Models;
+using System.Linq;
 
 namespace CafeOnline.Page
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repo)
         {
+            repository = repo;
         }
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
+
+        }
+
     }
 }
